6b23ea7dd78e4e61f1a9a8d8b5c95160e6870508
Amer0304 <60818537+Amer0304@users.noreply.github.com>
2021-07-28 20:57:43 +0200

    Finished CryptoWallet

---
+++ CryptoWalletApp/components/BalanceInfo.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import {Text, View, Image} from 'react-native';
+
+import {SIZES, COLORS, FONTS, icons} from '../constants';
+
+const BalanceInfo = ({title, displayAmount, changePct, containerStyle}) => {
+  return (
+    <View style={{...containerStyle}}>
+      {/* Title   */}
+      <Text style={{...FONTS.h3, color: COLORS.lightGray3}}>{title}</Text>
+
+      {/* Figures */}
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'flex-end',
+        }}>
+        <Text style={{...FONTS.h3, color: COLORS.lightGray3}}>$</Text>
+        <Text
+          style={{
+            ...FONTS.h2,
+            color: COLORS.white,
+            marginLeft: SIZES.base,
+            marginRight: SIZES.base,
+          }}>
+          {displayAmount.toLocaleString()}
+        </Text>
+        <Text style={{...FONTS.h3, color: COLORS.lightGray3}}>USD</Text>
+      </View>
+
+      {/* Change percentage */}
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'flex-end',
+        }}>
+        {changePct != 0 && (
+          <Image
+            source={icons.upArrow}
+            style={{
+              width: 10,
+              height: 10,
+              alignSelf: 'center',
+              tintColor: changePct > 0 ? COLORS.lightGreen : COLORS.red,
+              transform:
+                changePct > 0 ? [{rotate: '45deg'}] : [{rotate: '125deg'}],
+            }}
+          />
+        )}
+        <Text
+          style={{
+            marginLeft: SIZES.base,
+            alignSelf: 'flex-end',
+            color:
+              changePct == 0
+                ? COLORS.lightGray3
+                : changePct > 0
+                ? COLORS.lightGreen
+                : COLORS.red,
+            ...FONTS.h4,
+          }}>
+          {changePct.toFixed(2)}%
+        </Text>
+        <Text
+          style={{
+            marginLeft: SIZES.base,
+            alignSelf: 'flex-end',
+            color: COLORS.lightGray3,
+            ...FONTS.h3,
+          }}>
+          7d Change
+        </Text>
+      </View>
+    </View>
+  );
+};
+export default BalanceInfo;
